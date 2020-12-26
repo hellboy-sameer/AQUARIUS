f194a72cf068d20049b465ae7286ba96ea8b42c7
sameerpanthi <75129693+sameerpanthi@users.noreply.github.com>
2020-12-26 10:06:45 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,37 +1,74 @@
+# modify by @sameer-795
+# Kang with credits else gay.
+""" 
+Original Plugin By Darkcobra and Godhackerzuserbot
+Gv Credits Else Gey 
+"""
 import asyncio
+import os
+import requests
+import time
+from PIL import Image
+from io import BytesIO
+from datetime import datetime
+import random
 from telethon import events
-from uniborg.util import admin_cmd
+from userbot.utils import admin_cmd, sudo_cmd
 from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
+# 🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "LEGEND SAR"
+ALIVE_PHOTTO = os.environ.get("ALIVE_PHOTTO" , None)
 
-# uptime = get_readable_time((time.time() - Lastupdate))
+global ghanti
+ghanti = borg.uid
 
-PM_IMG = "https://telegra.ph/file/60c16f8f827b8e0929d3d.mp4"
+""" =======================CONSTANTS====================== """
+file1 = "https://telegra.ph/file/60c16f8f827b8e0929d3d.mp4"
+""" =======================CONSTANTS====================== """
+pm_caption = "__                   **🔥 丂卂ᐯ卂Ꮆ乇  🔥**  __\n\n"
 
-pm_caption = "__             **🔥 らΛƔΛƓƐ  🔥**  __\n\n"
-
-pm_caption += f"               __↼🄼🄰🅂🅃🄴🅁⇀__\n**『[{DEFAULTUSER}](tg://user?id={ghanti})』**\n\n"
+pm_caption += f"               __↼🄼🄰🅂🅃🄴🅁⇀__\n**      『[{DEFAULTUSER}](tg://user?id={ghanti})』**\n\n"
 pm_caption += "✘ ΔβØỮŦ Μ¥ Ş¥ŞŦ€Μ ✘\n\n"
 pm_caption += "➾ **𝐓𝐄𝐋𝐄𝐓𝐇𝐎𝐍**  ➣ 𝟏.𝟏𝟕.𝟓\n"
 pm_caption += "➾ **𝐒𝐔𝐏𝐏𝐎𝐑𝐓 ** ➣ [𝐉𝐎𝐈𝐍](https://t.me/joinchat/VhpqURitfHVMgXMeENMPJA)\n"
 pm_caption += "➾ **𝐒𝐎𝐂𝐈𝐀𝐋  **  ➣ [𝐉𝐎𝐈𝐍](https://t.me/joinchat/VhpqUT83rKwiDnghqupK8w)\n"
+pm_caption += "➾ **𝐂𝐇𝐀𝐍𝐍𝐄𝐋  **  ➣ [𝐉𝐎𝐈𝐍](https://t.me/joinchat/WCEEeeCSq4mE5KWm)\n"
 pm_caption += "➾ **𝐂𝐑𝐄𝐀𝐓𝐎𝐑** ➣ [⚡𝐒𝐀𝐌𝐄𝐄𝐑⚡](@SAMEER_795)\n" 
 
 pm_caption += " \n\n"
 pm_caption += "[✨ Đ€ƤŁØ¥ ¥ØỮŘ ŞΔVΔǤ€ ✨](https://github.com/sameerpanthi/SAVAGE)"
 
 
-
 @borg.on(admin_cmd(pattern=r"alive"))
-async def friday(alive):
-    chat = await alive.get_chat()
-    """ For .alive command, check if the bot is running.  """
-    await borg.send_file(alive.chat_id, PM_IMG,caption=pm_caption)
-    await alive.delete()
+@borg.on(sudo_cmd(pattern=r"alive", allow_sudo=True))
 
+async def amireallyalive(yes):
+    chat = await yes.get_chat()
+    global ghanti
+    ghanti = borg.uid
+##
+    on = await borg.send_file(yes.chat_id, file=file1,caption=pm_caption)
+
+    await asyncio.sleep(edit_time)
+    ok = await borg.edit_message(yes.chat_id, on, file=file2) 
+
+    await asyncio.sleep(edit_time)
+    ok2 = await borg.edit_message(yes.chat_id, ok, file=file3)
+
+    await asyncio.sleep(edit_time)
+    ok3 = await borg.edit_message(yes.chat_id, ok2, file=file1)
     
-@borg.on(admin_cmd(pattern=r"Alive", allow_sudo=True))
-async def friday(alive):
-    chat = await alive.get_chat()
-    """ For .alive command, check if the bot is running.  """
-    await borg.send_file(alive.chat_id, PM_IMG,caption=pm_caption)
+    await asyncio.sleep(edit_time)
+    ok4 = await borg.edit_message(yes.chat_id, ok3, file=file3)
+    
+    await asyncio.sleep(edit_time)
+    ok5 = await borg.edit_message(yes.chat_id, ok4, file=file2)
+    
+    await asyncio.sleep(edit_time)
+    ok6 = await borg.edit_message(yes.chat_id, ok5, file=file1)
+    
+    await asyncio.sleep(edit_time)
+    ok7 = await borg.edit_message(yes.chat_id, ok6, file=file4)
+
+    await yes.delete()
