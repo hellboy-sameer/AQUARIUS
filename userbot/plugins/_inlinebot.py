cf2f2903ea4b985629b013d651087fa0ffc1a343
hellboy-sameer <77003580+hellboy-sameer@users.noreply.github.com>
2021-01-11 21:35:39 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -85,7 +85,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             )
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
-        else
+        else:
             reply_pop_up_alert = "Kya daba rha h ... Jake apna khudka bot bna mera use naa kar....😏 @TEAM_AQUARIUS"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
